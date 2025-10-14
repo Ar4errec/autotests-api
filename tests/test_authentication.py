b0fd1a3398e7df68a3c3ea0ff43233eda06d17a6
Ar4errec <andreyandrey2517@gmail.com>
2025-10-14 23:25:48 +0300

    Фиктуры Pytest

--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -1,54 +1,28 @@
 from http import HTTPStatus
 import pytest
-from clients.authentication.authentication_client import get_authentication_client
+from clients.authentication.authentication_client import get_authentication_client, AuthenticationClient
 from clients.authentication.authentication_schema import LoginRequestSchema, LoginResponseSchema
-from clients.users.public_users_client import get_public_users_client
+from clients.users.public_users_client import get_public_users_client, PublicUsersClient
 from clients.users.users_schema import CreateUserRequestSchema
+from tests.conftest import UserFixture
 from tools.assertion.base import assert_status_code
 from tools.assertion.authentication import assert_login_response
 from tools.assertion.schema import validate_json_schema
-from tools.fakers import fake
 
 
 @pytest.mark.authentication
 @pytest.mark.regression
-def test_login():
-    """
-    Проверяет процесс аутентификации:
-    - создаёт нового пользователя,
-    - выполняет login,
-    - проверяет статус, схему и содержимое токенов.
-    """
+def test_login(function_user: UserFixture, authentication_client: AuthenticationClient):
 
-
-    public_client = get_public_users_client()
-    authentication_client = get_authentication_client()
-
-
-    email = fake.email()
-
-    create_user_request = CreateUserRequestSchema(
-        email=email,
-        password="string",
-        last_name="string",
-        first_name="string",
-        middle_name="string"
-    )
-    public_client.create_user(create_user_request)
-
-
-    login_request = LoginRequestSchema(
-        email=create_user_request.email,
-        password=create_user_request.password
-    )
-    login_response = authentication_client.login_api(login_request)
+    request = LoginRequestSchema(email=function_user.email, password=function_user.password)
+    response = authentication_client.login_api(request)
+    response_data = LoginResponseSchema.model_validate_json(response.text)
 
     # Assert: проверяем статус ответа
-    assert_status_code(login_response.status_code, HTTPStatus.OK)
+    assert_status_code(response.status_code, HTTPStatus.OK)
 
     # Assert: валидируем тело ответа
-    login_response_data = LoginResponseSchema.model_validate_json(login_response.text)
-    assert_login_response(login_response_data)
+    assert_login_response(response_data)
 
-    validate_json_schema(login_response.json(), login_response_data.model_json_schema())
+    validate_json_schema(response.json(), response_data.model_json_schema())
 
---
+++ tests/test_fixtures.py
@@ -0,0 +1,18 @@
+import pytest
+
+@pytest.fixture
+def user_client():
+    ...
+
+
+
+class TestUserFlow:
+
+
+    def test_user_can_login(self, user_client):
+        ...
+
+    def test_user_can_create_course(self, user_client):
+        ...
+
+
